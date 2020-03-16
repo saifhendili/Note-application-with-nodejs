1e3a85f91facf20793f7d0fd072730e5eb3d375b
saifhendili <57917635+saifhendili@users.noreply.github.com>
2020-03-16 11:31:02 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,26 +1,55 @@
-console.log('starting app')
-const yargs=require('yargs')
+	const yargs=require('yargs')
 let command=yargs.argv._[0]
 let nodes=require('./nodes.js')
 let title=yargs.argv.title
 let body=yargs.argv.body
+let unde
 if (process.argv.length < 3)
 	{ nodes.help();
 }
 else
 if(command==='add'){
+unde=process.argv[3]
+if(unde=='--title')
+{
 	console.log('adding node')
 	nodes.addNote(title, body);
+	
+}
+else{
+	console.log('Option : \nnode app.js add --title your_title --body todo_body \t to add a note')
+
+}
+	
 }else
 if(command==='remove'){
+	unde=process.argv[3]
+	if(unde=='--title')
+{
 	console.log('remove node')
 	nodes.removeNote(title)
+	
+}
+else{
+	console.log('Option : \nnode app.js remove --title your_title \t\t\t to remove a note')
+
+}
+
 }
 else
 if(command==='read'){
-
-	console.log('read node')
+	unde=process.argv[3]
+	if(unde=='--title')
+{
+console.log('read node')
 	nodes.readNote(title)
+	
+}
+else{
+	console.log('Option : \nnode app.js read --title your_title \t\t\t to read a note ')
+
+}
+	
 }
 else
 if(command==='list'){
